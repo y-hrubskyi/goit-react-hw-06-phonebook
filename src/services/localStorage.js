0bb1a34b20396843da650cff151873a5993e539b
y-hrubskyi <y.hrubskyi@gmail.com>
2023-11-16 17:27:19 +0200

    Delete localStorage.js

--- src/services/localStorage.js
+++
@@ -1,9 +0,0 @@
-const LS_KEY = 'contacts';
-
-export const getContactsFromLS = () => {
-  return JSON.parse(localStorage.getItem(LS_KEY)) ?? [];
-};
-
-export const setContactsToLS = contacts => {
-  localStorage.setItem(LS_KEY, JSON.stringify(contacts));
-};
