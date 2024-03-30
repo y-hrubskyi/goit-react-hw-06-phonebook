d8701e0f3cfa8b68a5a4594275908896179238e3
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-30 10:19:01 +0200

    refactor: separate ContactItem

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,35 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
-import toast from 'react-hot-toast';
+import { ContactItem } from 'components/ContactItem/ContactItem';
+import { ContactsList } from './ContactList.styled';
 
-import { getFilter } from 'redux/filterSlice';
-import { deleteContact, getContacts } from 'redux/contactsSlice';
-import { filterContacts } from 'helpers/filterContacts';
-
-import { ContactsList, ContactData, Button } from './ContactList.styled';
-
-export const ContactList = () => {
-  const dispatch = useDispatch();
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const deleteContactFoo = contactId => {
-    dispatch(deleteContact(contactId));
-    toast.success('Contact successfully deleted');
-  };
-
-  const filteredContacts = filterContacts(contacts, filter);
-
+export const ContactList = ({ contacts }) => {
   return (
     <ContactsList>
-      {filteredContacts.map(contact => (
-        <li key={contact.id}>
-          <ContactData>
-            {contact.name}: {contact.number}
-          </ContactData>
-          <Button type="button" onClick={() => deleteContactFoo(contact.id)}>
-            Delete
-          </Button>
-        </li>
+      {contacts.map(contact => (
+        <ContactItem key={contact.id} contact={contact} />
       ))}
     </ContactsList>
   );
